d9b037c975dbf113581ce0d1a1d938367ec7ccf8
nancyshenouda <Shenouda.nancy@gmail.com>
2021-05-25 19:23:42 -0400

    things we have done so far shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,12 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        // created a new project
+        // enable version control
+        // added .gitignore file
+        // made initial commit by keeping track of all files
+        // share project on GitHub
+
+
     }
 }
